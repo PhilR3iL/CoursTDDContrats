618b57564185b4a0b51c34e45f4e05d50355cdaf
proussille <proussille@petitdinosor>
2023-02-15 14:57:40 +0100

    Refactorisation.

--- src/bonjour/Bonjour.java
+++ src/bonjour/Bonjour.java
@@ -2,12 +2,8 @@ package bonjour;
 
 public class Bonjour {
 
-	public static String bonjour(String string) {
-		if (string.equals("Bernard"))
-			return "Bonjour, Bernard.";
-		if (string.equals("Alice"))
-			return "Bonjour, Alice.";
-		return "";
+	public static String bonjour(String nom) {
+		return "Bonjour, " + nom + ".";
 	}
 
 }
