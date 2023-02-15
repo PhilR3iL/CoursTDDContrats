f84544131db67c5b5755a5c0e53544b54d403328
proussille <proussille@petitdinosor>
2023-02-15 15:04:51 +0100

    Refactorisation.

--- src/bonjour/Bonjour.java
+++ src/bonjour/Bonjour.java
@@ -5,10 +5,8 @@ public class Bonjour {
 	public static String bonjour(String nom) {
 		if (nom == null)
 			return "Bonjour, ami.";
-		if (nom.equals("NORBERT"))
-			return "BONJOUR, NORBERT !";
-		else if (nom.equals("PATRICK"))
-			return "BONJOUR, PATRICK !";
+		if (nom.equals(nom.toUpperCase()))
+			return "BONJOUR, " + nom.toUpperCase() + " !";
 		return "Bonjour, " + nom + ".";
 	}
 
